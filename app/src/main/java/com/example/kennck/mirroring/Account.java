c10e16dce022ecce060ce1fcd83eaffc02863a63
kennCK <kennettecanales@gmail.com>
2018-06-25 00:42:42 +0800

    update

--- app/src/main/java/com/example/kennck/mirroring/Account.java
+++ app/src/main/java/com/example/kennck/mirroring/Account.java
@@ -1,16 +1,32 @@
 package com.example.kennck.mirroring;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -34,6 +50,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,7 +65,15 @@ public class Account extends AppCompatActivity implements AdapterView.OnItemClic
     ListView listView;
     Spinner menuSpinner;
     final int FILE_REQUEST = 1;
-    String mediaPath;
+    MediaProjectionManager mediaProjectionManager;
+    MediaProjection mediaProjection;
+    VirtualDisplay virtualDisplay;
+    final int RECORDCODEREQUEST = 500;
+    MediaRecorder mediaRecorder;
+    private final int WIDTH = 720;
+    private final int HEIGHT = 1280;
+    int screenDensity;
+    String filepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +89,8 @@ public class Account extends AppCompatActivity implements AdapterView.OnItemClic
         spinnerAdapter = new ArrayAdapter<String>(Account.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.spinner));
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
         menuSpinner.setAdapter(spinnerAdapter);
+        mediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        filepath = null;
         menuSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -70,7 +98,9 @@ public class Account extends AppCompatActivity implements AdapterView.OnItemClic
                     Intent askCode = new Intent(Account.this, CheckCode.class);
                     startActivity(askCode);
                 }else if(position == 2){
-                    // start recording
+                    askRecordPermission();
+                }else if(position == 3){
+                    stopRecording();
                 }
             }
 
@@ -91,6 +121,23 @@ public class Account extends AppCompatActivity implements AdapterView.OnItemClic
         });
         retrieveRecords();
         listView.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if(mediaRecorder != null){
+            stopRecording();
+        }
+        super.onDestroy();
+    }
+
+    public void askRecordPermission(){
+        if(mediaProjection == null){
+            startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), RECORDCODEREQUEST);
+        }else{
+            Log.v("Recording:", "Stop");
+        }
     }
 
     private void addFileButton() {
@@ -114,7 +161,9 @@ public class Account extends AppCompatActivity implements AdapterView.OnItemClic
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == FILE_REQUEST && resultCode == RESULT_OK && data != null){
             Uri uri = data.getData();
-
+        }else if(requestCode == RECORDCODEREQUEST && resultCode == RESULT_OK && data != null){
+            mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+            initMediaRecorder();
         }
     }
 
@@ -180,5 +229,78 @@ public class Account extends AppCompatActivity implements AdapterView.OnItemClic
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         TextView tv = (TextView)view;
         Toast.makeText(this, "Code: " + tv.getText() + position, Toast.LENGTH_LONG).show();
+    }
+
+    public void initMediaRecorder(){
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setVideoSize(WIDTH, HEIGHT);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mediaRecorder.setVideoEncodingBitRate(512 * 1000);
+        mediaRecorder.setVideoFrameRate(30);
+        mediaRecorder.setOutputFile(getFilePathDirectory());
+        prepareMediaRecorder();
+    }
+
+    public void getFilePath(){
+        File f;
+        do{
+            String filename = sharedpreferences.getString("username", null) + "_" + (System.currentTimeMillis()) + ".mp4";
+            filepath += "/Recorder/" + filename;
+            f = new File(filepath);
+        }while(f.exists() && !f.isDirectory());
+    }
+
+    public String getFilePathDirectory(){
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording/";
+        File dir = new File(path);
+        if(!dir.exists()){
+            dir.mkdir();
+        }else{
+            //
+        }
+        String filename = sharedpreferences.getString("username", null) + "_" + (System.currentTimeMillis()) + ".mp4";
+        return path + filename;
+    }
+
+    public void prepareMediaRecorder(){
+        try {
+            mediaRecorder.prepare();
+            Log.v("Recording:", "Preparing");
+            mediaRecorder.start();
+            Log.v("Recording:", "Started");
+            startRecording();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopRecording(){
+        if(mediaRecorder != null){
+            mediaProjection.stop();
+            mediaRecorder.release();
+            virtualDisplay.release();
+            virtualDisplay = null;
+            mediaRecorder = null;
+        }
+    }
+
+    public void startRecording(){
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        screenDensity = metrics.densityDpi;
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getRealSize(size);
+        virtualDisplay = mediaProjection.createVirtualDisplay("ScreenCapture",
+                WIDTH,
+                HEIGHT,
+                screenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.getSurface(),
+                null,
+                null);
     }
 }
